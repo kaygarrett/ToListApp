374e9cd0d7ad82595b485b705114525d08bc6001
Kayla <kaygarrett18@yahoo.com>
2020-07-28 14:47:43 -0700

    kayla

---
+++ ToListApp/ToDo.swift
@@ -0,0 +1,15 @@
+//
+//  ToDo.swift
+//  ToListApp
+//
+//  Created by Apple on 7/28/20.
+//  Copyright © 2020 Apple. All rights reserved.
+//
+
+import UIKit
+
+class ToDo {
+    var name = ""
+    var important = false
+}
+      
